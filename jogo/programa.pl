2f8e6c99ad9d9e769fbea8c6a8bddc50f4250e0d
Felipe-Dias-Mendes <48762331+Felipe-Dias-Mendes@users.noreply.github.com>
2019-05-20 22:29:19 -0300

    Update programa.pl

--- jogo/programa.pl
+++ jogo/programa.pl
@@ -17,5 +17,10 @@ subtrair([]).
 subtrair([X | Y]) :- X is X - 1,
 write(X), nl, subtrair(Y).
 
+%Função que pensei de subtrair
+sub([],0).
+sub([X], A):- A is X-1.
+sub([X | Xs], A) :- sub(X, ??), sub(Xs, ??). % Não o que eu passo em -> ??
+
 %exemplo matriz 3*3 ex no swi
 %imprimeLista([[1,2,3],[4,5,6],[7,8,9]]).
