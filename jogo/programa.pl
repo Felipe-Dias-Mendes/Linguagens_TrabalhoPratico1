de31508c00cf5b7697abc28dd3cd033a0c4e9031
Maycon Muller <maycon4muller5@gmail.com>
2019-05-20 22:13:15 -0300

    impressão de uma lista/matriz

---
+++ jogo/programa.pl
@@ -0,0 +1,21 @@
+pertence(X , [X|_]).
+pertence(X, [_|Y]):- pertence(X,Y).
+
+
+ultimo(X,[X]).
+ultimo(X, [_|Z]):- ultimo(X,Z).
+
+%impressão de uma lista qualquer
+%imprime a cabeça em seguida um new line
+
+imprimeLista([]).
+imprimeLista([X | Y]) :-
+write(X), nl, imprimeLista(Y).
+
+%não funciona
+subtrair([]).
+subtrair([X | Y]) :- X is X - 1,
+write(X), nl, subtrair(Y).
+
+%exemplo matriz 3*3 ex no swi
+%imprimeLista([[1,2,3],[4,5,6],[7,8,9]]).
