7a75bdcf0c5072cd0ea352b07a99a8011f2d4a33
Felipe-Dias-Mendes <48762331+Felipe-Dias-Mendes@users.noreply.github.com>
2019-06-02 10:57:35 -0300

    Update programa.pl

--- jogo/programa.pl
+++ jogo/programa.pl
@@ -1,16 +1,25 @@
+%Start
+%inicio(X,Y, Matrix, MatrixResultados).
+inicio(X,Y, Matrix, Resultado) :- procuraLista(X, Matrix, Linha),
+							subtrai(Y, Linha, Resultado), 
+    						imprimeLista(Resultado).
+
+
 %Aula 24/05
 procuraLista(0, [X|_], X).
-procuraLista(I, [X|Xs], E):- K is I-1, procuraLista(K, Xs, E).
+procuraLista(I, [_X|Xs], E):- K is I-1, procuraLista(K, Xs, E).
 
-procuraMatrix(X, Y, M, E):- procuraLista(X, M, Linha),
-							subtrai(Y, Linha, E), imprimeLista(M).
+inicio(X, Y, M):- procuraLista(X, M, Linha),
+							subtrai(Y, Linha, E), imprimeLista(E).
 
-subtrai(0, [X|_], R):- R is X-1.
-subtrai(I, [X|Xs], E):-  K is I-1, subtrai(K, Xs, E).
+subtrai(-1, Xs, Xs).
+subtrai(0, [X|Xs], [R|Ys]):- R is X-1, subtrai(-1, Xs, Ys).
+subtrai(I, [X|Xs], [X|Ys]):-  K is I-1, subtrai(K, Xs, Ys).
 
 imprimeLista([]).
 imprimeLista([X | Y]) :-
 write(X), nl, imprimeLista(Y).
+
 
 %AREA DE FUNÇÕES QUE FUNCIONAM ----------------------------------------------
 
